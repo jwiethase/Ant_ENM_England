9974f43a05a43daaf38caf0aa51230a957ebbacf
Joris Wiethase <j.wiethase@gmail.com>
2024-03-04 13:03:40 +0200

    Update maxent HPC script

--- R/HPC/Maxent_dismo_HPC.R
+++ R/HPC/Maxent_dismo_HPC.R
@@ -3,6 +3,9 @@ library(dismo)
 library(terra)
 library(tidyterra)
 library(raster)
+library(sf)
+library(tidyverse)
+library(ggpmisc)
 
 setwd('/users/jhw538/scratch/ant_modelling')
 source('source/misc_functions.R')
@@ -10,19 +13,19 @@ source('source/misc_functions.R')
 args <- commandArgs(trailingOnly=TRUE)
 job <- as.integer(args[1])
 
-dir.create(paste0("model_out/", gsub(" ", "_", species_choice)), showWarnings = F)
-dir.create(paste0("model_out/", gsub(" ", "_", species_choice), "/maxent"), showWarnings = F)
-dir.create(paste0("figures/", gsub(" ", "_", species_choice)), showWarnings = F)
-dir.create(paste0("figures/", gsub(" ", "_", species_choice), "/maxent"), showWarnings = F)
-
 # SET PARAMETERS ------------------------------------------
 species_choices <- c("Formica rufa", "Formica lugubris")
-smoother_list = c('tp', 'ps', 'cr')
-n_knots_list = c(3, 4)
+thin_list = c(100, 250, 500, 1000)
 
-mult_combs <- crossing(species_choices, smoother_list, n_knots_list) # 12
+mult_combs <- crossing(species_choices, thin_list) # 6
 comb_values <- mult_combs[job, ]
 print(comb_values)
+
+species_choice = comb_values$species_choices
+thin_dist = comb_values$thin_list 
+
+n_knots = 3
+smoother = "cr"
 
 covars_selection <- c("clim_topo_PC1_spline1", "clim_topo_PC1_spline2",
                       "clim_topo_PC2_spline1", "clim_topo_PC2_spline2",
@@ -35,8 +38,14 @@ covars_selection <- c("clim_topo_PC1_spline1", "clim_topo_PC1_spline2",
                       "lat_raster",
                       "forest_mask_buff")
 
+dir.create(paste0("model_out/", gsub(" ", "_", species_choice)), showWarnings = F)
+dir.create(paste0("model_out/", gsub(" ", "_", species_choice), "/maxent"), showWarnings = F)
+dir.create(paste0("figures/", gsub(" ", "_", species_choice)), showWarnings = F)
+dir.create(paste0("figures/", gsub(" ", "_", species_choice), "/maxent"), showWarnings = F)
+
+
 # DATA FILES ------------------------------------------
-ROI <- vect('spatial_other/ROI_outline_27700.shp') %>% 
+ROI <- vect('data/ROI_outline_27700.shp') %>% 
       terra::project(crs(km_proj))
 sporadic <- read.csv('data/sporadic_combined.csv') %>% 
       filter(species == species_choice) %>% 
@@ -55,8 +64,10 @@ effort_rast_10km <- raster('data/effort_rast_10km.tif') %>%
       disaggregate(fact = res(.)/res(clim_topo_covariates)) %>% 
       raster::resample(., raster(clim_topo_covariates), method = "bilinear")
 
-presences <- rbind(sporadic, exhaustive)
-thinned_presences <- gridSample(presences, predictors, n=1)
+thinned_presences <- rbind(sporadic, exhaustive) %>% 
+      vect(geom = c('x', 'y'), crs = crs(km_proj)) %>% 
+      thin_spatial(., dist_meters = thin_dist) %>% 
+      as.data.frame(geom = "XY")
 
 # PARTITION ------------------------------------------
 fold <- kfold(thinned_presences, k = 5)
